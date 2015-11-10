beb14ddebeb9cd04674c4fbb80c1091d93df019f
uchua <ilantzy@udel.edu>
2015-11-10 14:50:03 -0500

    Initial working version of momo.

--- src/momo.c
+++ src/momo.c
@@ -1,12 +1,35 @@
-#include <X11/Xlib.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <X11/Xlib.h>
+
+static int _XlibErrorHandler(Display *display, XErrorEvent *event) {
+  printf("Something bad happened.", stderr);
+  return True;
+}
 
 int main(int argc, char const *argv[]) {
+  int i, mo_x, mo_y, num_scrn, r, w_x, w_y;
+  unsigned int mr;
   Display *display;
-  Window root_event;
-  XEvent event;
+  Window window, wr;
 
-  
+  display = XOpenDisplay(0);
+  XSetErrorHandler(_XlibErrorHandler);
+  num_scrn = XScreenCount(display);
+
+  for (i = 0; i < num_scrn; i++) {
+    window = XRootWindow(display, i);
+    if ((r = XQueryPointer(display, window, &wr, &wr, &mo_x, &mo_y, &w_x, &w_y, &mr)) > 0) {
+      break;
+    }
+  }
+
+  if (r) {
+    printf("MoPos:(%d,%d)\n",mo_x,mo_y);
+  }
+  else {
+    fputs("ERROR\n",stdout);
+  }
 
   return 0;
 }
